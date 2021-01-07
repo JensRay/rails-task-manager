25d81b43c03751f2a696a67fdb04845c4f4800f9
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2021-01-07 19:48:43 +0100

    index, show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,17 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
 end
