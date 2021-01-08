0c29f74ef59d38ea741ec4761427bdaa3f86d486
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2021-01-08 20:29:31 +0100

    logic done. dry

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [ :show, :edit, :update, :destroy ]
   def index
     @tasks = Task.all
+  end
+
+  def show
   end
 
   def new
@@ -8,10 +12,35 @@ class TasksController < ApplicationController
   end
 
   def create
-
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path
   end
 
-  def show
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  def new_record?
+    @new_record
+  end
+
+  private
+
+  def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
